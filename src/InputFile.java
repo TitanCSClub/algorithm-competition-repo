25ed13e4bcb3af68cad7070ac9873d37117e4ba6
Arham4 <arhamjsiddiqui@gmail.com>
2017-12-10 09:47:13 -0600

    template files

---
+++ src/InputFile.java
@@ -0,0 +1,17 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+/**
+ * Template for inputting a file.
+ */
+public class InputFile {
+    private static final String FILE_NAME = "A.txt";
+
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner scanner = new Scanner(new File(FILE_NAME));
+        while (scanner.hasNext()) {
+
+        }
+    }
+}
---
+++ src/InputSystem.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+/**
+ * Template for inputting the system.
+ */
+public class InputSystem {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNext()) {
+
+        }
+    }
+}
